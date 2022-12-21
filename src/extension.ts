04457c2e4745c434afc3f021105d626dd4484661
Dheepak Krishnamurthy <me@kdheepak.com>
2022-12-21 14:43:48 -0500

    WIP

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,63 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import MarkdownIt = require("markdown-it");
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+interface Attributes {
+  [key: string]: string;
+}
+
+function _buildD2Attrs(fenceAttrs: string, tokenAttrs: [string, string][] | null): Attributes {
+  const attrs: Attributes = {};
+
+  const regexValidation = /^\s*\w+\s*=/;
+  if (regexValidation.test(fenceAttrs)) {
+    const regexKeyValue = /(\w+)\s*=\s*("([^"]*)"|'([^']*)'|([^"'\s]*))/g;
+    let match: RegExpExecArray | null = null;
+
+    while ((match = regexKeyValue.exec(fenceAttrs)) !== null) {
+      attrs[match[1].toLowerCase()] = match[2].replace(/^("|')(.*)\1$/, "$2");
+    }
+  }
+
+  tokenAttrs?.forEach((e) => {
+    attrs[e[0].toLowerCase()] = e[1];
+  });
+
+  return attrs;
+}
+
+export function matchD2Token(
+  tokenInfo: string,
+  tokenAttrs: [string, string][] | null,
+): Attributes | null {
+  const regexCurlyBrackets = /^\s*(d2)\s*((\{)([^`~]*)(\})\s*)?$/i;
+  const regexSingleMark = /^\s*(d2)\s*((:|\?)([^`~]*))?$/i;
+
+  const match = tokenInfo.match(regexCurlyBrackets) ?? tokenInfo.match(regexSingleMark);
+
+  return match !== null ? _buildD2Attrs(match[4], tokenAttrs) : null;
+}
+
 export function activate(context: vscode.ExtensionContext) {
+  return {
+    extendMarkdownIt(md: MarkdownIt) {
+      return md.use(markdownItD2);
+    },
+  };
+}
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "d2-markdown-preview" is now active!');
+function markdownItD2(md: MarkdownIt) {
+  const fallback = md.renderer.rules.fence;
 
-    return {
-        extendMarkdownIt(md: any) {
-            const highlight = md.options.highlight;
-            md.options.highlight = (code: any, lang: any) => {
-                if (lang && lang.match(/\bd2\b/i)) {
-                    return `<div class="d2">${code}</div>`;
-                }
-                return highlight(code, lang);
-            };
-            return md;
-        }
-    };
+  md.renderer.rules.fence = (tokens, idx, options, env, self) => {
+    const token = tokens[idx];
+    const attrs = matchD2Token(token.info, token.attrs);
+
+    console.log(attrs);
+
+    return fallback?.(tokens, idx, options, env, self) ?? `<pre>${token.content}</pre>`;
+  };
 }
 
 // This method is called when your extension is deactivated
