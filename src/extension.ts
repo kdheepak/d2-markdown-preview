7fd0f750051d44ad91bedb42fd02bb5e7cf014cd
Dheepak Krishnamurthy <me@kdheepak.com>
2022-12-21 18:57:30 -0500

    feat: Use core ruler ✨

--- src/extension.ts
+++ src/extension.ts
@@ -4,22 +4,41 @@
 import * as vscode from "vscode";
 import * as os from "os";
 import * as fs from "fs";
+import { execSync } from "child_process";
 import * as markdownIt from "markdown-it";
 
-function d2MarkdownItPlugin(md: markdownIt) {
-  const defaultRenderer = md.renderer.rules.fence;
-  md.renderer.rules.fence = function (tokens, idx, options, env, self) {
-    console.log("debugprint:", tokens);
+class D2Viz {
+  renderString(source: string) {
+    // Generate a unique file name
+    const now = `${Date.now()}`;
+    const tmpInFile = `${os.tmpdir()}/d2-${now}.d2`;
+    const tmpOutFile = `${os.tmpdir()}/d2-${now}.svg`;
 
-    const token = tokens[idx];
+    // Write to the temporary file
+    fs.writeFileSync(tmpInFile, source, "utf8");
 
-    if (token.info === "d2") {
-      const code = token.content.trim();
-      return defaultRenderer(tokens, idx, options, env, self);
-    } else {
-      return defaultRenderer(tokens, idx, options, env, self);
+    execSync(`d2 ${tmpInFile} ${tmpOutFile}`, { stdio: "inherit" });
+    const contents = fs.readFileSync(tmpOutFile, "utf8");
+    return contents;
+  }
+}
+
+function d2Worker(state: any) {
+  const viz = new D2Viz();
+  for (let i = 0; i < state.tokens.length; i++) {
+    const token = state.tokens[i];
+    if (token.type === "fence" && token.info === "d2") {
+      // Generate SVG output using the `d2` command line program
+      const output = viz.renderString(token.content);
+      // Replace the fenced code block token with an HTML block token containing the generated SVG output
+      state.tokens[i] = new state.Token("html_block", "", 0);
+      state.tokens[i].content = output;
     }
-  };
+  }
+}
+
+function d2MarkdownItPlugin(md: markdownIt) {
+  md.core.ruler.push("d2", d2Worker);
 }
 
 export function activate(context: vscode.ExtensionContext) {
