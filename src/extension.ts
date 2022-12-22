4a53bdafe4a5e51423c3158b523b1664abf83efe
Dheepak Krishnamurthy <me@kdheepak.com>
2022-12-22 09:36:44 -0500

    feat: Add options ✨

--- src/extension.ts
+++ src/extension.ts
@@ -6,29 +6,23 @@ import * as os from "os";
 import * as fs from "fs";
 import { execSync } from "child_process";
 import * as markdownIt from "markdown-it";
+import MarkdownIt = require("markdown-it");
 
 class D2Viz {
-  renderString(source: string) {
+  renderString(source: string, layout: string, theme: number) {
     const now = `${Date.now()}`;
     const tmpInFile = `${os.tmpdir()}/d2-${now}.d2`;
     const tmpOutFile = `${os.tmpdir()}/d2-${now}.svg`;
-
     fs.writeFileSync(tmpInFile, source, "utf8");
 
     try {
-      execSync(`d2 ${tmpInFile} ${tmpOutFile}`, {
+      execSync(`d2 --theme=${theme} --layout=${layout} ${tmpInFile} ${tmpOutFile}`, {
         windowsHide: true,
       });
     } catch (err) {
       // @ts-ignore
       const s = err.stderr.toString();
-      return `
-      <pre>
-      <code>
-      ${s}
-      </code>
-      </pre>
-      `;
+      return `<pre><code>${s}</code></pre>`;
     }
     const contents = fs.readFileSync(tmpOutFile, "utf8");
     return contents;
@@ -39,11 +33,19 @@ function d2Worker(state: any) {
   const viz = new D2Viz();
   for (let i = 0; i < state.tokens.length; i++) {
     const token = state.tokens[i];
-    if (token.type === "fence" && token.info === "d2") {
+    if (isD2Image(token)) {
       token.type = "d2";
       token.tag = "img";
     }
   }
+}
+
+function isD2Image(token: MarkdownIt.Token): boolean {
+  if (token.type !== "fence") {
+    return false;
+  }
+  let info = token.info.split(" ")[0];
+  return info === "d2";
 }
 
 // @ts-ignore
@@ -53,7 +55,28 @@ function renderD2Html(tokens, idx) {
   if (token.type !== "d2") {
     return token.content;
   }
-  const content = viz.renderString(token.content);
+  let layout = "elk";
+  let theme = 0;
+  if (token.info.split(" ").length !== 1) {
+    for (const elem of token.info.split(" ")) {
+      if (elem === "d2") {
+        continue;
+      }
+      if (elem.startsWith("layout")) {
+        layout = elem.split("=")[1];
+      }
+      if (elem.startsWith("theme")) {
+        theme = elem.split("=")[1].parseInt();
+      }
+    }
+  }
+  if (layout === null) {
+    layout = "elk";
+  }
+  if (Number.isNaN(theme) || theme === null) {
+    theme = 0;
+  }
+  const content = viz.renderString(token.content, layout, theme);
   return content;
 }
 
@@ -63,7 +86,7 @@ function d2MarkdownItPlugin(md: markdownIt) {
 }
 
 export function activate(context: vscode.ExtensionContext) {
-  console.log('debugprint: Congratulations, your extension "d2-markdown-preview" is now active!');
+  console.log('Congratulations, your extension "d2-markdown-preview" is now active!');
   return {
     extendMarkdownIt(md: markdownIt) {
       return md.use(d2MarkdownItPlugin);
